b8e5194b40e6e16c8374285015be8ea32c04384d
francesco-abritta <fra.abritta@gmail.com>
2022-01-27 15:26:54 +0100

    aggiunta classe global

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <header>
+
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    header{
+        height: 50px;
+        background-color: black;
+    }
+</style>
