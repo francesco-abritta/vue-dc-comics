10eca3a3c874807ed3cff117ed74616acce9d60e
francesco-abritta <fra.abritta@gmail.com>
2022-01-28 09:35:01 +0100

    prima sezione footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,30 @@
 <template>
     <div>
         <footer class="contenitore">
-
+            <Merchandise />
+            <Merchandise />
+            <Merchandise />
+            <Merchandise />
+            <Merchandise />
         </footer>
     </div>
 </template>
 
 <script>
+import Merchandise from './internFooter/Footer1/merchandise.vue'
+
 export default {
-    name: 'Footer'
+    name: 'Footer',
+    components: {
+        Merchandise
+    }
 }
 </script>
 
 <style>
-
+    footer{
+        background-color: #117CEC;
+        display: flex;
+        justify-content: space-between;
+    }
 </style>
---
+++ src/components/internFooter/Footer1/merchandise.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <div class="merc">
+          <section>
+            <img src="../../../assets/img/buy-comics-digital-comics.png" alt="">
+            DIGITAL COMICS
+          </section>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Merchandise'
+}
+</script>
+
+<style scoped>
+  @import './merchandise.scss';
+</style>
