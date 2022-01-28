5aced84b3a9e4d1c812cc25f63ff9bae97794caf
francesco-abritta <fra.abritta@gmail.com>
2022-01-28 19:13:25 +0100

    finito

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-      <main class="contenitore">
-        ciao
+      <div class="jumbo-main">
+      </div>
+      <main class="contenitore pad">
+        <button class="buttontop"><strong>CURRENT SERIES</strong></button>
+        <div class="items">
+            <div class="item" v-for="(elemento, indice) in items" :key="indice">
+                <img :src="elemento.thumb" alt="">
+                <div class="text">{{elemento.series}}</div>
+            </div>
+        </div>
+        <div class="bottone">
+            <button class="buttonbottom">LOAD MORE</button>
+        </div>
       </main>
   </div>
 </template>
